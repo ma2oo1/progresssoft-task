02f9076a800e54ba394785be7828ddc11eed74d9
shobe <sh.obeidat@outlook.com>
2025-04-26 22:01:40 +0300

    feat: add AutoMapper and setup its configuration.

---
+++ progresssoft-task.Server/Common/Mapping/MapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using progresssoft_task.Server.DTOs;
+using progresssoft_task.Server.Models;
+
+namespace progresssoft_task.Server.Common.Mapping
+{
+    public class MapperConfig
+    {
+        public static Mapper InitializeAutomapper() 
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<BusinessCard, BusinessCardDto>();
+                cfg.CreateMap<BusinessCard, BusinessCardDto>().ReverseMap();
+            });
+
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+    }
+}
