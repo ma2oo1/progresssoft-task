64a945aa117a05b7dc60fa38d3f277c24ffbd8e5
shobe <sh.obeidat@outlook.com>
2025-04-26 22:02:26 +0300

    feat: create BusinessCardDto

---
+++ progresssoft-task.Server/DTOs/BusinessCardDto.cs
@@ -0,0 +1,16 @@
+﻿using progresssoft_task.Server.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace progresssoft_task.Server.DTOs
+{
+    public class BusinessCardDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string? ProfilePic { get; set; }
+        public string Address { get; set; } = string.Empty;
+    }
+}
